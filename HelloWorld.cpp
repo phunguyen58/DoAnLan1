85537539f80e0d4ef3e33705df2bfb5018979436
phunguyen58 <nvphu0508@gmail.com>
2020-11-23 20:52:53 +0700

    Nguyen Van Phu fixed file cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
 	cout << "Hello World!\n";
-	cout << "This is project of Group 8";
+	cout << "This is project of Group 8\n";
+	cout << "1.Nguyen Van Phu";
 	return 0;
 }
