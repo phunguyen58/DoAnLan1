19a57e529029e65e1142bd7241c4c5a72f8d1671
duyqn2002 <duyqn2002@gmail.com>
2020-11-23 22:44:55 +0700

    Nguyen Duc Duy fixed

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
 	cout << "Hello World!\n";
 	cout << "This is project of Group 8\n";
-	cout << "1.Nguyen Van Phu";
+	cout << "1.Nguyen Van Phu\n";
+	cout << "2.Nguyen Duc Duy\n";
+	cout << "3.Tran Cam Quyen\n";
+	cout << "4.Bui Tien Dat\n";
+	cout << "5.Le Duy Tan";
 	return 0;
 }
